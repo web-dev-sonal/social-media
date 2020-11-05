736f85e3d5fb242736ab870ef31835539cbf6436
subham2000 <subham.kumar.min19@itbhu.ac.in>
2020-11-06 03:24:16 +0530

    populate and showing all post on home page

--- controller/home_controller.js
+++ controller/home_controller.js
@@ -1,10 +1,27 @@
+const posts = require('../models/post');
+
 module.exports.home = function(req,res){
     // return res.end('<h1> home page </h1>');
     //redirect to view engine
 
     // console.log(req.cookies);   //in every request we get some cookies
     // res.cookie('user_id',25);   //changing in cookie in server side
-    res.render('home',{
-        title: "Home"
-    });
+    // res.render('home',{
+    //     title: "Home",
+        
+    // });
+
+   /* posts.find({},function(err,post){
+        return res.render('home',{
+            title: "home",
+            posts: post  //int this post , user field will have only user id
+        });
+    })*/
+
+    posts.find({}).populate('user').exec(function(err,post){  //after populating now user field of post became user document which have full info about user
+        return res.render('home',{
+            title: "home",
+            posts: post
+        });
+    })
 }
